0834cdf42ad62f1659ea40d523acd6e52f3027fc
chalalala <neko_hime1212@yahoo.com>
2020-12-11 11:18:26 +0700

    Edit layout

---
+++ USTH-Weather/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
@@ -0,0 +1,59 @@
+package vn.edu.usth.weather;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+
+import android.util.Log;
+import android.widget.Toast; // Use Toast class to display message
+
+import android.os.Bundle;
+
+public class WeatherActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weather);
+
+        // Create a new fragment to be placed in the activity
+        ForecastFragment firstFragment = new ForecastFragment();
+
+        // Add the fragment to the 'container' FrameLayout
+        getSupportFragmentManager().beginTransaction().add(R.id.container, firstFragment).commit();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+//        Toast.makeText(getApplicationContext(),"Now onStart() calls", Toast.LENGTH_LONG).show(); //onStart Called
+        Log.i("WeatherActivity", "onStart"); // Send an INFO log message
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+//        Toast.makeText(getApplicationContext(),"Now onResume() calls", Toast.LENGTH_LONG).show();
+        Log.i("WeatherActivity", "onResume"); // Send an INFO log message
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+//        Toast.makeText(getApplicationContext(),"Now onPause() calls", Toast.LENGTH_LONG).show();
+        Log.i("WeatherActivity", "onPause"); // Send an INFO log message
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+//        Toast.makeText(getApplicationContext(),"Now onStop() calls", Toast.LENGTH_LONG).show();
+        Log.i("WeatherActivity", "onStop"); // Send an INFO log message
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+//        Toast.makeText(getApplicationContext(),"Now onDestroy() calls", Toast.LENGTH_LONG).show();
+        Log.i("WeatherActivity", "onDestroy"); // Send an INFO log message
+    }
+}
