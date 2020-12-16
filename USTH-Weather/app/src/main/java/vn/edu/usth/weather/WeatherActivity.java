ccdcba033560bc3690a91f18d9c749e36ce7a20d
chalalala <neko_hime1212@yahoo.com>
2020-12-16 18:07:49 +0700

    Add ToolBar with two actions

--- USTH-Weather/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
+++ USTH-Weather/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
@@ -7,8 +7,11 @@ import androidx.viewpager.widget.PagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 
 import android.content.Context;
+import android.content.Intent;
 import android.media.MediaPlayer;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.LinearLayout;
 import android.widget.Toast; // Use Toast class to display message
 
@@ -81,5 +84,31 @@ public class WeatherActivity extends AppCompatActivity {
         super.onDestroy();
 //        Toast.makeText(getApplicationContext(),"Now onDestroy() calls", Toast.LENGTH_LONG).show();
         Log.i("WeatherActivity", "onDestroy"); // Send an INFO log message
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_weather, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_refresh:
+            {
+                Toast.makeText(getApplicationContext(), "Refreshing...", Toast.LENGTH_SHORT).show();
+                return true;
+            }
+            case R.id.action_settings:
+            {
+                Intent intent = new Intent(this, PrefActivity.class);
+                startActivity(intent);
+                return true;
+            }
+            default:
+                super.onOptionsItemSelected(item);
+        }
+        return false;
     }
 }
